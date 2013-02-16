df58e96ab3512bea515b2fb819969fea66d97138
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2013-02-16 10:39:53 -0800

    initial commit of submodules and associated scripts

---
+++ update-submodules.sh
@@ -0,0 +1,13 @@
+#! /bin/bash -e
+
+git submodule foreach --recursive git submodule init
+git submodule foreach --recursive git submodule update
+
+( 
+    while read submodule branch; do
+        echo updating $submodule to branch $branch
+        ( cd "$submodule" && git checkout "$branch" && git fetch origin && git merge "origin/$branch" )
+        echo done
+    done 
+) <.submodule-branches
+
